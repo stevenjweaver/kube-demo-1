502dc1f54e089b86da972b097bb380d186cd25ff
Steven Weaver <sjweaver@us.ibm.com>
2017-05-15 14:36:54 -0500

    changed namespace

--- deploy-to-kube.sh
+++ deploy-to-kube.sh
@@ -1,10 +1,8 @@
 rm -rf node_modules
-export DOCKER_HOST=tcp://containers-api.ng.bluemix.net:8443
-export DOCKER_CERT_PATH=/home/pipeline/.ice/certs/containers-api.ng.bluemix.net/8d42a0bb-3329-43f5-bc62-139f29de7c5c 	export DOCKER_TLS_VERIFY=1
 bx ic init
-docker build --tag registry.ng.bluemix.net/antonal/kube-demo-sw:latest .
+bx ic build --tag registry.ng.bluemix.net/sjweaver/kube-demo-sw:latest .
 bx cr login
-docker push registry.ng.bluemix.net/antonal/kube-demo-sw:latest
+docker push registry.ng.bluemix.net/sjweaver/kube-demo-sw:latest
 kubectl create -f kube-all-in-one.yaml
 kubectl get service kube-demo-sw-service
 kubectl get nodes
