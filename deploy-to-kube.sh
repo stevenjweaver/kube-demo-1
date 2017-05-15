75e580752926288019ff0a11f567b9c869daf205
Steven Weaver <sjweaver@us.ibm.com>
2017-05-15 15:03:54 -0500

    changed manifest

--- deploy-to-kube.sh
+++ deploy-to-kube.sh
@@ -2,7 +2,7 @@ rm -rf node_modules
 bx ic init
 bx ic build --tag registry.ng.bluemix.net/sjweaver/kube-demo-sw:latest .
 bx cr login
-docker push registry.ng.bluemix.net/sjweaver/kube-demo-sw:latest
+# docker push registry.ng.bluemix.net/sjweaver/kube-demo-sw:latest
 kubectl create -f kube-all-in-one.yaml
 kubectl get service kube-demo-sw-service
 kubectl get nodes
