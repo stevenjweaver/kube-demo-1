7caecbc4d0554ee0bcf5d70ba5a9ccfdb78a6320
Steven Weaver <sjweaver@us.ibm.com>
2017-05-15 15:27:56 -0500

    removed docker

--- deploy-to-kube.sh
+++ deploy-to-kube.sh
@@ -1,8 +1,6 @@
 rm -rf node_modules
 bx ic init
-# bx ic build --tag registry.ng.bluemix.net/sjweaver/kube-demo-sw:latest .
-# bx cr login
-# docker push registry.ng.bluemix.net/sjweaver/kube-demo-sw:latest
+
 kubectl create -f kube-all-in-one.yaml
 kubectl get service kube-demo-sw-service
 kubectl get nodes
