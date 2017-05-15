48d043d0564d03d78f739661f2310bf5af268b20
Steven Weaver <sjweaver@us.ibm.com>
2017-05-15 09:16:36 -0500

    add files to Git

---
+++ kubedemosw/deploy-to-kube.sh
@@ -0,0 +1,8 @@
+rm -rf node_modules
+docker build --tag registry.ng.bluemix.net/antonal/kube-demo-sw:latest .
+bx cr login
+docker push registry.ng.bluemix.net/antonal/kube-demo-sw:latest
+kubectl create -f kube-all-in-one.yaml
+kubectl get service kube-demo-sw-service
+kubectl get nodes
+echo Use one of the nodes IP addresses and the port exposed by kube-demo-sw-service 
